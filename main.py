1641b05de7f434d84f991b7be930bafdfe5a37fa
Ouiame4 <makhoukhouiam@gmail.com>
2025-08-30 12:03:04 +0100

    main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ async def analyze_json(payload: JSONData):
 
     # Add values above each point
     for x, y in zip(mentions_over_time.index.astype(str), mentions_over_time.values):
-        ax1.text(x, y + 0.23, str(y), ha='center', fontsize=9)
+        ax1.text(x, y + 0.25, str(y), ha='center', fontsize=9)
 
     evolution_mentions_b64 = fig_to_base64(fig1)
     plt.close(fig1)
